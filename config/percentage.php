5049652de2174c552393ba2bd4e6282c882cf321
tiennh <tiennh@amaisoft.com>
2022-11-08 16:28:54 +0700

    [FixBug][USER_005] - Percent infor user

--- config/percentage.php
+++ config/percentage.php
@@ -2,17 +2,30 @@
 
 return [
     'default' => 0,
-    'information' => 50,
-    'favorite' => 3,
-    'self_pr' => 4,
-    'pr' => 10,
-    'experience' => 3,
+    'information' => [
+        'total' => 50,
+        'attribute_required' => 9,
+    ],
+    'pr' => [
+        'total' => 10,
+        'attribute' => [
+            'favorite' => 3,
+            'self_pr' => 4,
+            'experience' => 3,
+        ]
+    ],
     'motivation' => 5,
     'user_learning_history' => 15,
     'user_qualification' => 5,
-    'user_work_history' => 7,
-    'business_content' => 4,
-    'work_history' => 4,
-    'experience_accumulation' => 4,
-    'complete' => 1,
+    'work_history' => [
+        'total' => 15,
+        'attribute' => [
+            'store_name' => 1,
+            'job_type_id' => 1,
+            'position_office_ids' => 4,
+            'business_content' => 4,
+            'experience_accumulation' => 4,
+            'period_start' => 1,
+        ],
+    ],
 ];
