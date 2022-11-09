51fbf8d796972d036aa706403edd4b2f9db2fea8
nguyenvan khang <khangnv@amaisoft.com>
2022-11-09 16:12:20 +0700

    [Fixbug][USER] - Upload image

--- app/Http/Requests/Recruiter/UploadImageRequest.php
+++ app/Http/Requests/Recruiter/UploadImageRequest.php
@@ -24,7 +24,7 @@ class UploadImageRequest extends FormRequest
     public function rules()
     {
         return [
-            'image' => ['required', 'image', 'mimes:jpeg,png', 'mimetypes:image/jpeg,image/png', 'max:' . config('upload.size_max')],
+            'image' => ['required', 'image', 'mimes:jpg,jpeg,png,svg', 'mimetypes:image/jpeg,image/png,image/jpg,image/svg', 'max:' . config('upload.size_max')],
         ];
     }
 
